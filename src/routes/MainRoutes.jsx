9920734dbf53b045b694afc04ee7637e641158ae
prachi301s <prachisahu2711@gmail.com>
2024-04-05 19:03:22 +0530

    navbar added

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+// import { Route, Routes } from 'react-router-dom'
+// import About from '../sections/About'
+
+// const MainRoutes = () => {
+//   return (
+//    <>
+//    <Routes>
+    
+//     <Route path="/about" element={<About/>} />
+//     <Route path="/contact" element={<Contact />} />
+//     <Route path="/projects" element={<Projects />} />
+   
+//    </Routes>
+//    </>
+//   )
+// }
+
+// export default MainRoutes
